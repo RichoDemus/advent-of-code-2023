89a2b539aa0a6397f6df83b389335a1c1743ec02
RichoDemus <git@richodemus.com>
2023-12-01 11:00:06 +0100

    Add skeleton

---
+++ src/template.rs
@@ -0,0 +1,44 @@
+#[aoc(dayX, part1)]
+fn part1(input: &str) -> usize {
+    todo!()
+}
+
+// #[aoc(dayX, part2)]
+// fn part2(input: &str) -> usize {
+//     todo!()
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+// #[test]
+    // fn verify_part1() {
+    //     let input = include_str!("../input/2022/dayX.txt");
+    //     assert_eq!(part1(input), 0);
+    // }
+
+    // #[test]
+    // fn verify_part2() {
+    //     let input = include_str!("../input/2022/dayX.txt");
+    //     assert_eq!(part2(input), 0);
+    // }
+
+    #[test]
+    fn part1_provided_example() {
+        let result = part1(
+            r#""#,
+        );
+
+        assert_eq!(result, 0)
+    }
+
+    // #[test]
+    // fn part2_provided_example() {
+    //     let result = part2(
+    //         r#""#,
+    //     );
+    //
+    //     assert_eq!(result, 0)
+    // }
+}
